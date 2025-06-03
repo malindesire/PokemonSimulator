096bde4009f509474aa3048e0e8d72a8cf263eb4
malindesire <malindesirejohansson@gmail.com>
2025-06-03 16:47:50 +0200

    Create Attack class

---
+++ Game/Attack.cs
@@ -0,0 +1,24 @@
+﻿namespace Game
+{
+    internal class Attack
+    {
+        public string Name { get; }
+        public ElementType Type { get; }
+        public int BasePower { get; }
+
+        public Attack(string name, ElementType type, int basePower) 
+        {
+            Name = name;
+            Type = type;
+            BasePower = basePower;
+        }
+
+        public void Use(int level)
+        {
+            int TotalPower = BasePower + level;
+
+            Console.WriteLine($"{Name} hits with total power {TotalPower}!");
+
+        }
+    }
+}
