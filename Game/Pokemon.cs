73d4e7b65cab4a89389771ef53370144e33924e0
malindesire <malindesirejohansson@gmail.com>
2025-06-03 17:09:43 +0200

    Add Pokemon abstract class

---
+++ Game/Pokemon.cs
@@ -0,0 +1,17 @@
+﻿namespace Game
+{
+    internal abstract class Pokemon
+    {
+        public string Name { get; }
+        public int Level { get; private set; }
+        public ElementType Type { get; }
+        public List<Attack> Attacks { get; }
+        public Pokemon(string name, int level, ElementType type, List<Attack> attacks)
+        {
+            Name = name;
+            Level = level;
+            Type = type;
+            Attacks = attacks ?? new List<Attack>();
+        }
+    }
+}
