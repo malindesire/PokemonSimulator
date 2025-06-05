161c62dcdc353a45cb5282c3ef09351006f2c5ca
malindesire <malindesirejohansson@gmail.com>
2025-06-05 14:19:11 +0200

    Make Squirtle evolvable

--- Game/Pokemons/Squirtle.cs
+++ Game/Pokemons/Squirtle.cs
@@ -2,9 +2,18 @@
 
 namespace Game.Pokemons
 {
-    internal class Squirtle : WaterPokemon
+    internal class Squirtle : WaterPokemon, IEvolvable
     {
         public Squirtle(int level, List<Attack> attacks) : base("Squirtle", level, attacks)
         { }
+
+        public void Evolve()
+        {
+            string oldName = Name; // Store the old name
+            Name = "Wartortle";
+            Level += 10; // Evolving increases the level by 10
+
+            Console.WriteLine($"{oldName} is evolving... Now it's {Name}! Level {Level}!");
+        }
     }
 }
