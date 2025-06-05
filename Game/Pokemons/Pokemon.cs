c098955aefee968f0930da36a7343f7d998d084c
malindesire <malindesirejohansson@gmail.com>
2025-06-05 13:11:51 +0200

    Revert "Handle invalid input in Attack method"
    This reverts commit 6540c3bbbf5d5747a1014562f005eb6d847660e8.

--- Game/Pokemons/Pokemon.cs
+++ Game/Pokemons/Pokemon.cs
@@ -26,30 +26,14 @@
         public void Attack()
         {
             // Let the user pick an attack from the list of attacks and invoke its Use method.
-            Console.WriteLine($"Choose an attack for {Name} by selecting a number:");
+            Console.WriteLine($"Choose an attack for {Name}:");
             for (int i = 0; i < Attacks.Count; i++)
             {
                 Console.WriteLine($"{i + 1}. {Attacks[i].Name}");
             }
-            
-            bool success = int.TryParse(Console.ReadLine(), out int choice);
-            choice -= 1; // Adjust for zero-based index
-
-            if (success) 
-            {
-                if (choice >= 0 && choice < Attacks.Count)
-                {
-                    Attacks[choice].Use(Level);
-                }
-                else
-                {
-                    Console.WriteLine("That choice doesn't exist");
-                }
-            }
-            else 
-            {
-                Console.WriteLine("Invalid input, you must choose a number.");
-            }
+            int choice = int.Parse(Console.ReadLine() ?? string.Empty) - 1; // ToDo: Handle if input is invalid
+           
+            Attacks[choice].Use(Level);
         }
 
         public void RaiseLevel()
